27b9b54bf9921a02804baa75991ad73ea8e145fe
stech <sourabhgupta9509@gmail.com>
2024-01-05 00:03:55 +0530

    done

---
+++ 1st time/index.js
@@ -0,0 +1,5 @@
+let name = "this is me";
+let greeting =" developer ";
+let message =  name +  greeting ;
+console.log(message);
+alert("hello");
---
+++ 3rd time/index.js
@@ -0,0 +1,3 @@
+alert("hello , welcome to the my first websites !")
+isFOLLOW = "false";
+console.log(isFOLLOW);
